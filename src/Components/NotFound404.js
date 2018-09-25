ae010169cb3477ca58b747055ee27085542fa096
DeveloperJW <zpw1992@gmail.com>
2018-09-24 22:11:54 -0500

    Modified 404 error page

--- src/Components/NotFound404.js
+++ src/Components/NotFound404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const NotFound404 = () => {
     return (
@@ -6,6 +7,7 @@ const NotFound404 = () => {
             <li className='not-found'>
                 <h3>Error 404: No Results Found</h3>
                 <p>You search did not return any results. Please try again.</p>
+                <NavLink to="/">Click here to go back to home page</NavLink>
             </li>
         </ul>
     )
